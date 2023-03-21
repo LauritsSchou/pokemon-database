dd8a411479bd29161defcd9bb41a704e62b4cfe2
LauritsSchou <laurits.schou@gmail.com>
2023-03-21 15:27:10 +0100

    property values and detailview

--- script.js
+++ script.js
@@ -22,4 +22,28 @@ const psyduck = {
   statsSpecialDefence: 5,
   statsSpeed: 5.5,
 };
-console.log(psyduck);
+function showPokemon(pokemon) {
+  const pokemonHTML = /*html*/ `
+  <li>Image: <img src="${pokemon.image}"></li>
+  <li>Footprint: <img src="${pokemon.footprint}"></li>
+  <li>Name: ${pokemon.name}</li> 
+  <li>Description: ${pokemon.description}</li>
+  <li>Ability: ${pokemon.ability}</li>
+  <li>Pokédex index: 00${pokemon.dexindex}</li>
+  <li>Type: ${pokemon.type}</li>
+  <li>Subtype: ${pokemon.subtype}</li>
+  <li>Weaknesses: ${pokemon.weaknesses}</li>
+  <li>Gender: ${pokemon.gender}</li>
+  <li>Weight: ${pokemon.weight} grams</li>
+  <li>Height: ${pokemon.height} centimeters</li>
+  <li>Generation: ${pokemon.generation}</li>
+  <li>Game version: ${pokemon.spilversion}</li>
+  <li>Can it evolve: ${pokemon.canEvolve}</li>
+  <li>HP: ${pokemon.statsHP}</li>
+  <li>Attack: ${pokemon.statsAttack}</li>
+  <li>Special attack: ${pokemon.statsSpecialAttack}</li>
+  <li>Special defence: ${pokemon.statsSpecialDefence}</li>
+  <li>Speed: ${pokemon.statsSpeed}</li> `;
+  document.querySelector("#pokemonlist").insertAdjacentHTML("beforeend", pokemonHTML);
+}
+showPokemon(psyduck);
