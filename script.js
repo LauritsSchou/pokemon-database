c1ce32238463880a293b99434e94f5ff8298b8b6
LauritsSchou <laurits.schou@gmail.com>
2023-03-22 13:00:18 +0100

    dialog and css

--- script.js
+++ script.js
@@ -1,33 +1,55 @@
 "use strict";
-const psyduck = {
-  name: "Psyduck",
-  description: "It is constantly wracked by a headache. When the headache turns intense, it begins using mysterious powers.",
-  ability: "Damp, Cloud Nine",
-  image: "https://archives.bulbagarden.net/media/upload/thumb/3/3f/0054Psyduck.png/375px-0054Psyduck.png",
-  footprint: "https://archives.bulbagarden.net/media/upload/b/ba/F0054.png",
-  dexindex: 54,
-  type: "Water",
-  subtype: null,
-  weaknesses: "Grass, Electric",
-  gender: "Male or female",
-  weight: 1960,
-  height: 80,
-  generation: 1,
-  spilversion: "Yellow/Red/Blue",
-  canEvolve: true,
-  statsHP: 5,
-  statsAttack: 5.2,
-  statsDefence: 4.8,
-  statsSpecialAttack: 6.5,
-  statsSpecialDefence: 5,
-  statsSpeed: 5.5,
-};
+window.addEventListener("load", initApp);
+function initApp() {
+  const psyduck = {
+    name: "Psyduck",
+    description:
+      "It is constantly wracked by a headache. When the headache turns intense, it begins using mysterious powers.",
+    ability: "Damp, Cloud Nine",
+    image:
+      "https://archives.bulbagarden.net/media/upload/thumb/3/3f/0054Psyduck.png/375px-0054Psyduck.png",
+    footprint: "https://archives.bulbagarden.net/media/upload/b/ba/F0054.png",
+    dexindex: 54,
+    type: "Water",
+    subtype: null,
+    weaknesses: "Grass, Electric",
+    gender: "Male or female",
+    weight: 1960,
+    height: 80,
+    generation: 1,
+    spilversion: "Yellow/Red/Blue",
+    canEvolve: true,
+    statsHP: 5,
+    statsAttack: 5.2,
+    statsDefence: 4.8,
+    statsSpecialAttack: 6.5,
+    statsSpecialDefence: 5,
+    statsSpeed: 5.5,
+  };
+
+  showPokemon(psyduck);
+}
+
 function showPokemon(pokemon) {
-  const pokemonHTML = /*html*/ `
-  <li>Image: <img src="${pokemon.image}"></li>
+  const pokemonHTML = /*html*/ ` <article class="grid-item">
+                <img src="${pokemon.image}">
+                <h2>${pokemon.name}</h2>
+                <p>${pokemon.type}</p>
+            </article>`;
+  document
+    .querySelector("#pokemon")
+    .insertAdjacentHTML("beforeend", pokemonHTML);
+
+  document
+    .querySelector("#pokemon article:last-child")
+    .addEventListener("click", pokemonClicked);
+  function pokemonClicked() {
+    document.querySelector("#pokemondetails").showModal();
+    const dialogHTML = /*html*/ `
+    <h1>${pokemon.name}</h1>
+<img src="${pokemon.image}"></li>
+<h3><i>"${pokemon.description}"</i></h3>
   <li>Footprint: <img src="${pokemon.footprint}"></li>
-  <li>Name: ${pokemon.name}</li> 
-  <li>Description: ${pokemon.description}</li>
   <li>Ability: ${pokemon.ability}</li>
   <li>Pokédex index: #00${pokemon.dexindex}</li>
   <li>Type: ${pokemon.type}</li>
@@ -43,7 +65,12 @@ function showPokemon(pokemon) {
   <li>Attack: ${pokemon.statsAttack}</li>
   <li>Special attack: ${pokemon.statsSpecialAttack}</li>
   <li>Special defence: ${pokemon.statsSpecialDefence}</li>
-  <li>Speed: ${pokemon.statsSpeed}</li> `;
-  document.querySelector("#pokemonlist").insertAdjacentHTML("beforeend", pokemonHTML);
+  <li>Speed: ${pokemon.statsSpeed}</li> 
+  <form method="dialog">
+		<button>Close</button>
+</form>`;
+    document
+      .querySelector("#pokemondetails")
+      .insertAdjacentHTML("beforeend", dialogHTML);
+  }
 }
-showPokemon(psyduck);
